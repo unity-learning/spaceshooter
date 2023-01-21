d13a1279aaebb5c500e65fc47289cec407983d0c
miladimos <milad.jafari6210@gmail.com>
2023-01-22 02:09:42 +0330

    Add Asteroids

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -7,21 +7,31 @@ public class AsteroidController : MonoBehaviour
     public float speed;
     public GameObject asteroid;
     public float rotationSpeed;
-    public float health;
+    public int health;
     public GameObject explosionPrefab;
+    public Sprite[] healthSprite;
 
 
+    private Animator anim;
+    private string ANIMATION_NAME = "health";
+    private SpriteRenderer spriteRender;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
-        
+
     }
 
 
     void Update()
     {
         transform.position += Vector3.down * speed * Time.deltaTime;
-
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
 
     }
@@ -34,11 +44,32 @@ public class AsteroidController : MonoBehaviour
 
     private void CheckHealth()
     {
-        if (health <= 0) {
+        if (health <= 0)
+        {
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
-        
+        } else
+        {
+            DoAnimationOrChangeSprite();
         }
     }
+
+    private void ChangeSprite()
+    {
+        spriteRender.sprite = healthSprite[health - 1];
+    }
+
+    private void DoAnimationOrChangeSprite()
+    {
+        if (anim != null)
+        {
+            anim.SetInteger(ANIMATION_NAME, health);
+        }
+        else
+        {
+            ChangeSprite();
+        }
+    }
+
 
 }

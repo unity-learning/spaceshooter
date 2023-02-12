60440e8733ee2d2e27c40bd8ddee874a5f0ba726
miladimos <milad.jafari6210@gmail.com>
2023-02-13 01:12:02 +0330

    add base repository file

---
+++ Assets/Scripts/Repositories/CoinRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRepository : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
